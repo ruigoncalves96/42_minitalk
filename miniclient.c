d40675821af3607a69122c565404b67d543b28b9
Rui Alexandre Andrade Goncalves <randrade@c2r8s2.42lisboa.com>
2024-09-25 16:55:46 +0100

    25/09 16:55

--- miniclient.c
+++ miniclient.c
@@ -6,41 +6,75 @@
 /*   By: randrade <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 16:36:02 by randrade          #+#    #+#             */
-/*   Updated: 2024/09/25 12:15:53 by randrade         ###   ########.fr       */
+/*   Updated: 2024/09/25 16:46:32 by randrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	ft_getbit(unsigned int num, int position)
+int	ft_parsing_check(int argc, char *str)
 {
-	return ((num >> position) & 1);
+	int	i;
+
+	if (argc != 3)
+	{
+		ft_putstr_fd("\n -xxx- Wrong format! Please, try again. -xxx-\n\n", 1);
+		return (1);
+	}
+	i = 0;
+	while (str[i])
+	{
+		if (ft_isdigit(str[i]) != 1)
+		{
+			ft_putstr_fd("\n -xxx- Wrong PID Format! Please, try again. ", 1);
+			ft_putstr_fd("-xxx-\n\n", 1);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
 }
 
-int	ft_sendbit(char *str, int pid)
+int	ft_sendbit(unsigned int c, int pid)
+{
+	int	i;
+
+	i = 7;
+	while (i >= 0)
+	{
+		if (((c >> i) & 1) == 1)
+		{
+			if (kill(pid, SIGUSR1) != 0)
+				return (1);
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) != 0)
+				return (1);
+		}
+		usleep(10);
+		i--;
+	}
+	return (0);
+}
+
+int	ft_sendmessage(char *str, int pid)
 {
 	int	i;
 
 	while (*str)
 	{
-		i = 7;
-		while (i >= 0)
-		{
-			if (ft_getbit(*str, i) == 1)
-				kill(pid, SIGUSR1);
-			else
-				kill(pid, SIGUSR2);
-			i--;
-			usleep(50);
-		}
+		if (ft_sendbit(*str, pid) != 0)
+			return (1);
 		str++;
 	}
 	i = 0;
 	while (i < 8)
 	{
-		kill(pid, SIGUSR2);
+		if (kill(pid, SIGUSR2) != 0)
+			return (1);
+		usleep(10);
 		i++;
-		usleep(50);
 	}
 	return (0);
 }
@@ -55,27 +89,19 @@ int	main(int argc, char *argv[])
 {
 	int	pid;
 
-	if (argc != 3)
-	{
-		ft_putstr_fd("Wrong format\n", 1);
+	if (ft_parsing_check(argc, argv[1]) == 1)
 		return (1);
-	}
-//	while (*argv[1])
-//	{
-//		if (ft_isdigit(*argv[1]) != 1)
-//			return (1);
-//		(argv[1])++;
-//	}
 	signal(SIGUSR1, &ft_signal_handle);
 	pid = ft_atoi(argv[1]);
-	if (kill(pid, 0) != 0)
+	if (pid <= 0 || kill(pid, 0) != 0)
 	{
-		ft_putstr_fd("Invalid PID, try again\n", 1);
+		ft_putstr_fd("\n-xxx- Wrong PID connection! Please, try again. ", 1);
+		ft_putstr_fd("-xxx-\n\n", 1);
 		return (1);
 	}
-	if (ft_sendbit(argv[2], pid) != 0)
+	if (ft_sendmessage(argv[2], pid) != 0)
 	{
-		printf("Failed to send message! Please try again.\n");
+		printf("\n-xxx- Failed to send message! Please, try again. -xxx-\n\n");
 		return (1);
 	}
 	return (0);
